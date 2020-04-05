a38162d21e1f1e06eaff8c8baed35f78943c0c67
Haowen Xu <haowen.xu@outlook.com>
2020-04-05 14:40:12 +0800

    updated CI script

---
+++ .github/workflows/unittest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# set the work-dir ro /prj
+cd /prj
+
+# Install the dependencies
+pip install -r requirements-dev.txt
+
+# Report the package versions
+python --version
+pytest --version
+coveralls --version
+
+# Test with PyTorch 1.3.1
+pip install "torch==1.3.1"
+
+export TENSORKIT_BACKEND=PyTorch TENSORKIT_JIT_MODE=none TENSORKIT_VALIDATE_TENSORS=false
+pytest --cov=tensorkit --cov-append
+
+export TENSORKIT_BACKEND=PyTorch TENSORKIT_JIT_MODE=none TENSORKIT_VALIDATE_TENSORS=true
+pytest --cov=tensorkit --cov-append
+
+export TENSORKIT_BACKEND=PyTorch TENSORKIT_JIT_MODE=all TENSORKIT_VALIDATE_TENSORS=false
+pytest --cov=tensorkit --cov-append
+
+# submit the result to coveralls.io
+coveralls
