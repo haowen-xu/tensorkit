148f5347adf783381b5b3dcd94111881b7e6c6a2
Haowen Xu <haowen.xu@outlook.com>
2021-11-20 17:59:18 +0800

    fix GitHub actions

--- .github/workflows/unittest.sh
+++ .github/workflows/unittest.sh
@@ -7,6 +7,7 @@ cd /prj
 
 # Install the dependencies
 pip install -r requirements-dev.txt
+pip install git+https://github.com/haowen-xu/ml-essentials.git
 
 # Report the package versions
 python --version
@@ -14,7 +15,7 @@ pytest --version
 coveralls --version
 
 # Test with PyTorch 1.7.1
-pip install "torch==1.7.1" "torchvision==0.8.2"
+pip install "torch==1.10.0" "torchvision==0.11.1"
 
 export TENSORKIT_BACKEND=PyTorch TENSORKIT_JIT_MODE=none TENSORKIT_VALIDATE_TENSORS=false
 pytest --cov=tensorkit --cov-append
